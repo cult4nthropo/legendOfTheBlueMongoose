8f0e7fe01d262c0a628ab3bcefadb762c49a885b
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2022-03-05 22:59:26 +0100

    created array of matching enemies for fighting

--- legend.js
+++ legend.js
@@ -1,43 +1,43 @@
 var you;
 
 var enemies = [
-	{name: "Giant Rat", level: 1, healthpoints: 20, armor: "none", weapon: "Bites", lootLevel: 1},
-	{name: "Wolf", level: 2, healthpoints: 25, armor: "none",  weapon: "Bites", lootLevel: 1},
-	{name: "Bandit", level: 2, healthpoints: 45, armor: "Clothes", weapon: "Dagger", lootLevel: 2},
-	{name: "Giant Spider", level: 3, healthpoints: 50, armor: "none", weapon: "Bites", lootLevel: 2},
-	{name: "Lake Constance Mosquito", level: 4, healthpoints: 50, armor: "none", weapon: "Bites", lootLevel: 1},
-	{name: "Goblin", level: 3, healthpoints: 25, armor: "none", weapon: "Arrow", lootLevel:2},
-	{name: "Black Knight", level: 4, healthpoints: 50, armor: "Steel Armor", weapon: "Long Sword", lootLevel: 4},
-	{name: "Tree Monster", level: 4, healthpoints: 75, armor: "none", weapon: "Roots", lootLevel: 4},
-	{name: "Genie", level: 5, healthpoints: 80, armor: "none", weapon: "Magic Spell", lootLevel: 5},
-	{name: "Mongoose Magician", level: 5, healthpoints: 80, armor: "Mage Robe", weapon: "Magic Spell", lootLevel: 5},
-	{name: "Blue Mongoose Knight", level: 6, healthpoints: 90, armor: "Cursed Armor", weapon: "Poisoned Sword", lootLevel: 6},
-	{name: "Saber Tooth Tiger", level: 6, healthpoints: 110, armor: "none", weapon: "Saber Tooth", lootLevel: 6},
-	{name: "The Blue Mongoose", level: 7, healthpoints: 150, armor: "Chitinous Shell", weapon: "Poison Ray", lootLevel: 7}
+	{enemyType: "Giant Rat", level: 1, healthpoints: 20, armor: "None", weapon: "Bites", lootLevel: 1},
+	{enemyType: "Wolf", level: 2, healthpoints: 25, armor: "None",  weapon: "Bites", lootLevel: 1},
+	{enemyType: "Bandit", level: 2, healthpoints: 45, armor: "Clothes", weapon: "Dagger", lootLevel: 2},
+	{enemyType: "Giant Spider", level: 3, healthpoints: 50, armor: "None", weapon: "Bites", lootLevel: 2},
+	{enemyType: "Lake Constance Mosquito", level: 4, healthpoints: 50, armor: "None", weapon: "Bites", lootLevel: 1},
+	{enemyType: "Goblin", level: 3, healthpoints: 25, armor: "None", weapon: "Arrow", lootLevel:2},
+	{enemyType: "Black Knight", level: 4, healthpoints: 50, armor: "Steel Armor", weapon: "Long Sword", lootLevel: 4},
+	{enemyType: "Tree Monster", level: 4, healthpoints: 75, armor: "None", weapon: "Roots", lootLevel: 4},
+	{enemyType: "Genie", level: 5, healthpoints: 80, armor: "None", weapon: "Spell", lootLevel: 5},
+	{enemyType: "Mongoose Magician", level: 5, healthpoints: 80, armor: "Mage Robe", weapon: "Spell", lootLevel: 5},
+	{enemyType: "Blue Mongoose Knight", level: 6, healthpoints: 90, armor: "Enchanted Leather Armor", weapon: "Poisoned Sword", lootLevel: 6},
+	{enemyType: "Saber Tooth Tiger", level: 6, healthpoints: 110, armor: "None", weapon: "Saber Tooth", lootLevel: 6},
+	{enemyType: "The Blue Mongoose", level: 7, healthpoints: 150, armor: "Chitinous Shell", weapon: "Poison Ray", lootLevel: 7}
 ];
 
 var weapons = [
-	{weaponType: "Bites", damage: [1, 5], weaponLevel: 1, cost: 0},
-	{weaponType: "Poison Ray", damage: [7,20], weaponLevel: 7, cost: 0},
-	{weaponType: "Fists", damage: [1, 5], weaponLevel: 1, cost: 0},
+	{weaponType: "Bites", damage: [1, 5], weaponLevel: 0, cost: 0},
+	{weaponType: "Poison Ray", damage: [7,20], weaponLevel: 0, cost: 0},
+	{weaponType: "Fists", damage: [1, 5], weaponLevel: 0, cost: 0},
 	{weaponType: "Dagger", damage: [3,7], weaponLevel: 2, cost: 25},
 	{weaponType: "Short Sword", damage: [5, 10], weaponLevel: 3, cost: 75},
 	{weaponType: "Long Sword", damage: [7, 12], weaponLevel: 4, cost: 125},
 	{weaponType: "Arrow", damage: [3, 7], weaponLevel: 3, cost: 15},
-	{weaponType: "Roots", damage: [5, 8], weaponLevel: 4, cost: 0},
-	{weaponType: "Magic Spell", damage: [7, 9], weaponLevel: 5, cost: 100},
+	{weaponType: "Roots", damage: [5, 8], weaponLevel: 0, cost: 0},
+	{weaponType: "Spell", damage: [7, 9], weaponLevel: 0, cost: 100},
 	{weaponType: "Poisoned Sword", damage: [11, 15], weaponLevel: 6, cost: 150},
-	{weaponType: "Saber Tooth", damage: [11, 15], weaponLevel: 6, cost: 0}
+	{weaponType: "Saber Tooth", damage: [11, 15], weaponLevel: 0, cost: 0}
 ];
 
 var armor = [
-	{armorType: "none", protection: 0, cost: 0},
+	{armorType: "None", protection: 0, cost: 0},
 	{armorType: "Chitinous Shell", protection: 10, cost: 0},
 	{armorType: "Clothes", protection: 1, cost: 0},
 	{armorType: "Leather Armor", protection: 2, cost: 50},
 	{armorType: "Studded Leather Armor", protection: 4, cost: 200},
 	{armorType: "Steel Armor", protection: 7, cost: 400},
-	{armorType: "Cursed Armor", protection: 15, cost: 500},
+	{armorType: "Enchanted Leather Armor", protection: 15, cost: 500},
 	{armorType: "Mage Robe", protection: 15, cost: 400}
 ]
 
@@ -152,6 +152,7 @@ function init() {
 	}
 	updateStatusbar();
 	
+	//delete afterwards. right now it's only here for debugging purposes
 	localStorage.setItem("Hero", you);
 }
 
@@ -166,7 +167,49 @@ function updateStatusbar () {
 	" | armor: " + you.getArmorType();
 }
 
+function checkForLevelUpProgress () {
+	var levelUp = [0, 300, 600, 1200, 2400, 4800, 9600, 19200]
+	var currentXp = you.getXp();
+	for (var i = 0; i < levelUp.length; i ++) {
+		if (currentXp >= levelUp[i]) {
+			var currentLevel = i+1;
+			you.setLevel(currentLevel);
+			updateStatusbar();
+			localStorage.setItem("Hero", you);
+		}
+	}
+	var nextLevelProgress = (currentXp / levelUp[you.getLevel()]) ;
+	return nextLevelProgress;
+}
+
 function startFighting() {
+	/**
+	 * Choose an enemy that matches your current level
+	 * Therefore:
+	 * 1. get your level with level progress ->done
+	 * Therefore: create an array or formula to define your level progress which will be defined by your XP -> done
+	 * 			  If you are about 15% (the return value of checkForLevelUpProgress() has to be at least 0.85) before hitting the next level the enemy could have your level + 1
+	 * 2. Create an enemy array with matching enemies -> done
+	 * 3. Chose a random anemy from this array
+	 * 4. write the enemy with status bar and specific story in the document
+	 */
+
+	var fightingLevel = you.getLevel();
+	console.log(fightingLevel);
+	var levelProgress = checkForLevelUpProgress();
+	var enemiesToFight = [];
+	 if (levelProgress >= 0.85) {
+		 fightingLevel +=1;
+	 }
+
+	for(var i = 0; i < enemies.length; i++) {
+		
+		if (enemies[i].level <= fightingLevel) {
+			console.log(enemies[i].level)
+			enemiesToFight.push(enemies[i]);
+		}
+	}
+	console.log(enemiesToFight);
 	
 }
 
